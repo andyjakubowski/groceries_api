40a89ed9987fb498bd1f548a09224069266a39c1
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-01 18:04:37 +0200

    Whitelist parameters

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,9 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  private
+    def item_params
+      params.require(:item).permit(:id, :order_id, :text, :is_completed, :is_open)
+    end
 end
