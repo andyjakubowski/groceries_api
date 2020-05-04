c6ee6c5e5e42767bb684c25512607e9496addf86
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-04 10:58:42 +0200

    Add naive items#create implementation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,10 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def create
+    Item.create(item_params)
+  end
+
   private
     def item_params
       params.require(:item).permit(:id, :order_id, :text, :is_completed, :is_open)
