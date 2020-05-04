c1be90e79fdf29f407861b6aa206c875062afa5c
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-04 11:21:53 +0200

    Add naive items#destroy implementation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,11 @@ class ItemsController < ApplicationController
     item.update(item_params)
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
   private
     def item_params
       params.require(:item).permit(:id, :order_id, :text, :is_completed, :is_open)
