b62b7ef5fe77c362278a4a80924f464ee223fe3c
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-04 11:17:32 +0200

    Add naive items#update implementation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,11 @@ class ItemsController < ApplicationController
     Item.create(item_params)
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
   private
     def item_params
       params.require(:item).permit(:id, :order_id, :text, :is_completed, :is_open)
