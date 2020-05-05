d95b38bf0f8934171e826afa4afee12c9a0e8342
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-05 14:43:27 +0200

    Set up Action Cable ListChannel

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,17 +4,49 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    item = Item.create(item_params)
+
+    ActionCable.server.broadcast(
+      "ListChannel",
+      message_type: "item_create",
+      item: {
+        id: item.id,
+        orderId: item.order_id,
+        text: item.text,
+        isCompleted: item.is_completed,
+        isOpen: item.is_open
+      }
+    )
   end
 
   def update
     item = Item.find(params[:id])
     item.update(item_params)
+
+    ActionCable.server.broadcast(
+      "ListChannel",
+      message_type: "item_update",
+      item: {
+        id: item.id,
+        orderId: item.order_id,
+        text: item.text,
+        isCompleted: item.is_completed,
+        isOpen: item.is_open
+      }
+    )
   end
 
   def destroy
     item = Item.find(params[:id])
     item.destroy
+
+    ActionCable.server.broadcast(
+      "ListChannel",
+      message_type: "item_delete",
+      item: {
+        id: item.id
+      }
+    )
   end
 
   private
