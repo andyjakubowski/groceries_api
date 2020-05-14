a28e590a93e3f1c5b48a0366eeaa27afd71c40cc
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-14 17:54:50 +0200

    Add route for updating many items at once

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,25 @@ class ItemsController < ApplicationController
     )
   end
 
+  def update_many
+    many_items_params do |parameters|
+      item = Item.find(parameters[:id])
+      item.update(parameters)
+
+      ActionCable.server.broadcast(
+        "ListChannel",
+        message_type: "item_update",
+        item: {
+          id: item.id,
+          orderId: item.order_id,
+          text: item.text,
+          isCompleted: item.is_completed,
+        },
+        clientId: params[:client_id].presence
+      )
+    end
+  end
+
   def destroy
     item = Item.find(params[:id])
     item.destroy
@@ -51,13 +70,20 @@ class ItemsController < ApplicationController
   end
 
   private
-    def item_params
-      params
+    def item_params(parameters = params)
+      parameters
         .require(:item)
         .permit(
           :id,
           :order_id,
           :text,
           :is_completed)
+    end
+
+    def many_items_params
+      params.require(:items).each do |single_item_params|
+        permitted_params = single_item_params.permit(:id, :order_id, :text, :is_completed)
+        yield permitted_params
+      end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :items
+  resources :items do
+    collection do
+      put 'update_many'
+    end
+  end
+
 end
